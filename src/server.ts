9669015e2be8cde0d0dc30f3b10c901f06e77edc
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-10-01 01:15:11 +0300

    use rate limiter to prevent possible attacks

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import helmet from "helmet";
 import mongoSanitize from "express-mongo-sanitize";
 import xss from "xss-clean";
+import rateLimit from "express-rate-limit";
 
 const { connectDB } = require("./database/mongodb");
 
@@ -26,6 +27,13 @@ app.use(mongoSanitize());
 //Data sanitization against XSS attacks
 app.use(xss());
 
+//Set rate limit to requests coming from same API.
+const limiter = rateLimit({
+  max: 100,
+  //An API can make maximum 100 requests in a hour 
+  windowMs: 60 * 60 * 100,
+  message: "Too many requests from this IP. Please try again later!"
+})
 
 app.use(cors(corsOptions));
 
